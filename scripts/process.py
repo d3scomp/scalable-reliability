b60b226c05f78f58719313ddbde6b393f4bebe6b
Vladimir Matena <matena@d3s.mff.cuni.cz>
2016-07-28 15:55:14 +0200

    Plot speed histogram

--- scripts/process.py
+++ scripts/process.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 import matplotlib
 
 matplotlib.use('Agg')
@@ -9,45 +10,30 @@ from matplotlib import pyplot, colors
 import numpy as np
 
 
-def processLogFile(file: str):
-	print("Loading " + file)
-	f = open(file, 'r')
+def getspeed(name, colliders):
+	if name.startswith("humans"):
+		mymaxspeed = 1
+	else:
+		mymaxspeed = 5
 
-	lattencies = []
-	distancies = []
-	boxLattencies = {}
-	data = []
+	period = 0.030
+	decelleration = 5
 
-	while True:
+	resspeed = mymaxspeed
 
-		# Read reported name
-		name = f.readline()
+	for collider in colliders:
+		dist = collider['dist']
+		remdist = dist - collider['latt'] * collider['speed']
+		remdist = max(0, remdist)
+		collidespeed = math.sqrt(2 * remdist * decelleration)
+		myspeed = max(0, collidespeed - collider['speed'])
+		resspeed = min(resspeed, myspeed)
 
-		if name == '':
-			break
+	return resspeed
 
-		while True:
-			line = f.readline()
-			if len(line.rstrip()) == 0:
-				break
 
-			# Process line
-			vals = line.split("\t")
-			latt = float(vals[0])
-			gtdist = float(vals[1])
-			dist = float(vals[2])
-
-			lattencies.append(latt)
-			distancies.append(gtdist)
-
-			dist_m = int(gtdist)
-			if dist_m not in boxLattencies:
-				boxLattencies[dist_m] = []
-
-			boxLattencies[dist_m].append(latt)
-
-	print("Processing " + file)
-
+def printDelays(file, boxLattencies):
+	# Delays
 	fig = plot.figure()
 	ax = fig.add_subplot(111)
 
@@ -76,6 +62,70 @@ def processLogFile(file: str):
 
 	print("Storing eps")
 	fig.savefig(file + ".eps")
+
+def printSpeeds(file, speeds):
+	# Speeds hist
+	fig = plot.figure()
+	ax = fig.add_subplot(111)
+	ax.set_xlabel("Robot speed in meters per second")
+	ax.set_ylabel("Samples")
+	ax.hist(speeds, 50)
+	print("Storing png")
+	fig.savefig(file + ".speedhist.png", dpi=256, width=20, wight=15)
+	print("Storing eps")
+	fig.savefig(file + ".speedhist.eps")
+
+
+def processLogFile(file: str):
+	print("Loading " + file)
+	f = open(file, 'r')
+
+	lattencies = []
+	distancies = []
+	speeds = []
+	boxLattencies = {}
+	data = []
+
+	while True:
+		# Read reported name
+		name = f.readline()
+
+		if name == '':
+			break
+
+		colliders = []
+
+		while True:
+			line = f.readline()
+			if len(line.rstrip()) == 0:
+				break
+
+			# Process line
+			vals = line.split("\t")
+			latt = float(vals[0])
+			gtdist = float(vals[1])
+			dist = float(vals[2])
+			maxspeed = float(vals[3])
+
+			lattencies.append(latt)
+			distancies.append(gtdist)
+
+			colliders.append({"dist": dist, "speed": maxspeed, "latt": latt})
+
+			dist_m = int(gtdist)
+			if dist_m not in boxLattencies:
+				boxLattencies[dist_m] = []
+
+			boxLattencies[dist_m].append(latt)
+
+		if name.startswith("robot"):
+			speeds.append(getspeed(name, colliders))
+
+	print("Processing " + file)
+
+	printDelays(file, boxLattencies)
+
+	printSpeeds(file, speeds)
 
 
 # fig.savefig(file + ".pdf")
